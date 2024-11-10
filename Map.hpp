e4e886eee177e6726aeac2c292e5f5214cc2e6c4
ion4444 <baxanean48@gmail.com>
2024-11-10 15:17:00 +0200

    Update Map.hpp

--- Map.hpp
+++ Map.hpp
@@ -1,18 +1,12 @@
-// Map.hpp #ifndef MAP_HPP #define MAP_HPP
+#pragma once
+#include <vector>
+#include <string>
 
-class Map { public: static const int width = 20; static const int height = 10; char map[height][width]; // 2D array for the game map
+class Map {
+public:
+    Map();
+    void displayMap() const;
 
-// Constructor to initialize the map
-Map();
-
-// Function to draw the map
-void drawMap(int playerX, int playerY, int score) const;
-
-// Check if the given position is a wall
-bool isWall(int x, int y) const;
-
-// Set the map element at (x, y)
-void setMapElement(int x, int y, char element);
+private:
+    std::vector<std::string> tiles;
 };
-
-#endif
