8b4d338ec9870d1194defad14e4e6a94619a0483
ion4444 <baxanean48@gmail.com>
2024-12-05 10:51:15 +0200

    Update Player.hpp

--- Player.hpp
+++ Player.hpp
@@ -1,12 +1,40 @@
+/**
+ * @file player.hpp
+ * @brief Declarația clasei Player.
+ * 
+ * Proiect: Game Framework  
+ * Autor: [Baxanean Ionuț]  
+ * Data: [05.12.24]  
+ * 
+ * Clasa Player reprezintă un jucător cu funcționalități de bază.
+ */
+
 #pragma once
 #include <string>
 
+/**
+ * @class Player
+ * @brief Reprezintă un jucător în cadrul jocului.
+ */
 class Player {
 public:
+    /**
+     * @brief Constructorul clasei Player.
+     * @param name Numele jucătorului.
+     */
     explicit Player(const std::string& name);
+
+    /**
+     * @brief Simulează mișcarea jucătorului.
+     */
     void move();
+
+    /**
+     * @brief Returnează numele jucătorului.
+     * @return Numele jucătorului.
+     */
     std::string getName() const;
 
 private:
-    std::string name;
+    std::string name; /**< Numele jucătorului. */
 };
